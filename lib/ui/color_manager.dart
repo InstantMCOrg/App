b3433d68f04f80f35c7a851a1955b7a2daedbf3d
Binozo <70137898+Binozo@users.noreply.github.com>
2023-02-23 21:08:04 +0100

    added colors to color_manager.dart

--- lib/ui/color_manager.dart
+++ lib/ui/color_manager.dart
@@ -5,7 +5,11 @@ class ColorManager {
   final Color secondary;
   final Color accent;
 
-  final Color error = const Color.fromRGBO(255, 51, 51, 1);
+  static const Color error = Color.fromRGBO(255, 51, 51, 1);
+  final Color cardBackgroundColor = const Color.fromRGBO(59, 58, 65, 1);
+  static const Color white = Color.fromRGBO(255, 255, 255, 1);
+  static const Color black = Color.fromRGBO(21, 19, 28, 1);
+
   ColorManager.dark() :
         primary = const Color.fromRGBO(21, 27, 41, 1),
         secondary = const Color.fromRGBO(31, 47, 85, 1),
