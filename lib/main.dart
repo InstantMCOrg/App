47bfed79ebadc4373cf8d8b00eb96a284d2cc864
Binozo <70137898+Binozo@users.noreply.github.com>
2023-02-23 21:10:59 +0100

    setup main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,40 @@
 import 'package:InstantMC/constants/config.dart';
+import 'package:InstantMC/resources/cubits/login_steps_switcher/login_steps_switcher_cubit.dart';
+import 'package:InstantMC/resources/cubits/start/start_cubit.dart';
+import 'package:InstantMC/resources/repositories/storage_repository.dart';
 import 'package:InstantMC/resources/router.dart';
 import 'package:InstantMC/ui/theme_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(const InstantMC());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(InstantMC());
 }
 
 class InstantMC extends StatelessWidget {
-  const InstantMC({Key? key}) : super(key: key);
+  final StorageRepository _storageRepository = StorageRepository();
+  late final StartCubit _startCubit;
+  late final LoginStepsSwitcherCubit _loginStepsSwitcherCubit;
+
+  InstantMC({Key? key}) : super(key: key) {
+    _startCubit = StartCubit(_storageRepository);
+    _loginStepsSwitcherCubit = LoginStepsSwitcherCubit();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: kAppName,
-      initialRoute: "/",
-      onGenerateRoute: AppRouter.route,
-      theme: ThemeManager.dark(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => _startCubit),
+        BlocProvider(create: (_) => _loginStepsSwitcherCubit),
+      ],
+      child: MaterialApp(
+        title: kAppName,
+        initialRoute: "/",
+        onGenerateRoute: AppRouter.route,
+        theme: ThemeManager.dark(),
+      ),
     );
   }
 }
