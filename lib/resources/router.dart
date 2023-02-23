6a79d42ec64404ad2698932342a06ad342a5c7f9
Binozo <70137898+Binozo@users.noreply.github.com>
2023-02-23 21:10:52 +0100

    added routing stuff

--- lib/resources/router.dart
+++ lib/resources/router.dart
@@ -1,10 +1,16 @@
 import 'package:InstantMC/resources/routes.dart';
 import 'package:InstantMC/ui/screens/home_screen.dart';
+import 'package:InstantMC/ui/screens/login_screen.dart';
+import 'package:InstantMC/ui/screens/start_screen.dart';
 import 'package:flutter/material.dart';
 
 class AppRouter {
   static Route route(RouteSettings routeSettings) {
     switch(routeSettings.name) {
+      case Routes.start:
+        return MaterialPageRoute(builder: (_) => const StartScreen());
+      case Routes.login:
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
       case Routes.home:
         return MaterialPageRoute(builder: (_) =>  const HomeScreen());
     }
--- lib/resources/routes.dart
+++ lib/resources/routes.dart
@@ -1,4 +1,5 @@
 class Routes {
-  static const String home = "/";
+  static const String start = "/";
+  static const String home = "/home";
   static const String login = "/login";
 }
