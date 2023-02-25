0aa5233870272597ff9ac93d8d3b7997084fddbd
Binozo <70137898+Binozo@users.noreply.github.com>
2023-02-25 13:18:37 +0100

    added password change and dashboard screen

--- lib/resources/router.dart
+++ lib/resources/router.dart
@@ -1,6 +1,8 @@
 import 'package:InstantMC/resources/routes.dart';
+import 'package:InstantMC/ui/screens/dashboard_screen.dart';
 import 'package:InstantMC/ui/screens/home_screen.dart';
 import 'package:InstantMC/ui/screens/login_screen.dart';
+import 'package:InstantMC/ui/screens/required_password_change_screen.dart';
 import 'package:InstantMC/ui/screens/start_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -13,6 +15,10 @@ class AppRouter {
         return MaterialPageRoute(builder: (_) => const LoginScreen());
       case Routes.home:
         return MaterialPageRoute(builder: (_) =>  const HomeScreen());
+      case Routes.dashboard:
+        return MaterialPageRoute(builder: (_) =>  const DashboardScreen());
+      case Routes.passwordChange:
+        return MaterialPageRoute(builder: (_) =>  const RequiredPasswordChangeScreen());
     }
     // default route
     return MaterialPageRoute(builder: (_) =>  const HomeScreen());
--- lib/resources/routes.dart
+++ lib/resources/routes.dart
@@ -2,4 +2,6 @@ class Routes {
   static const String start = "/";
   static const String home = "/home";
   static const String login = "/login";
+  static const String dashboard = "/dashboard";
+  static const String passwordChange = "/passwordChange";
 }
